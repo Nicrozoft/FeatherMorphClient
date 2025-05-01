091e715f1920294eee3dc25dcc6735234c728068
MATRIX-feather <midnightcarnival@outlook.com>
2025-05-01 16:10:12 +0800

    misc: Bump command compat layer

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
     modImplementation("com.terraformersmc:modmenu:${project.property("modmenu_version")}")
     modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_version")}")
 
-    val compatLayerVersion = "8177fbdad6"
+    val compatLayerVersion = "8da9ebb5ab"
 
     modImplementation("com.github.NiFeather:feathermorph-command-compat-layer:$compatLayerVersion")
     include("com.github.NiFeather:feathermorph-command-compat-layer:$compatLayerVersion")
