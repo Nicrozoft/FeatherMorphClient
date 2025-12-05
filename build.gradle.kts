421e1648aca55858a0882e5287cf6e854bb573b2
MATRIX-feather <midnightcarnival@outlook.com>
2025-12-05 13:40:22 +0800

    misc: Prepare for 1.21.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("fabric-loom") version "1.11-SNAPSHOT" apply false
+    id("net.fabricmc.fabric-loom-remap") version "1.14-SNAPSHOT" apply false
     id("net.neoforged.moddev") version "2.0.93" apply false
     id("maven-publish")
 }
