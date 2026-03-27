7721904db75de8ba74a876b3e3f32ea13c2638ca
MATRIX-feather <midnightcarnival@outlook.com>
2026-03-27 11:20:31 +0800

    misc: Prepare for 26.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("net.fabricmc.fabric-loom-remap") version "1.14-SNAPSHOT" apply false
+    id("net.fabricmc.fabric-loom") version "1.15-SNAPSHOT" apply false
     id("net.neoforged.moddev") version "2.0.93" apply false
     id("maven-publish")
 }
